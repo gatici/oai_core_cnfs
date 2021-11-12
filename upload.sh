bca0ca68a7b7b1dcf8d03dba00884d02c817dc4b
Gulsum Atici <gulsumatici@gmail.com>
2021-11-12 20:44:35 +0300

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
-osm upload-package oai_cn5g_cnf
-osm upload-package oai_cn5g_ns
+tar -czvf oai_cn5g_cnf.tar.gz oai_cn5g_cnf; osm vnfd-create oai_cn5g_cnf.tar.gz
+tar -czvf oai_cn5g_ns.tar.gz oai_cn5g_ns; osm nsd-create oai_cn5g_ns.tar.gz
